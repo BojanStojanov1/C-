2b435ca973a77a3c3007d0a63c3379002e9b7a2c
aaaa <bojan.20406@gmail.com>
2025-03-08 15:18:30 +0100

    homework

---
+++ StudentGroup/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Make a new console application called StudentGroup
+Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names.
+Get a number from the console between 1 and 2 and print the students from that group in the console.
+Ex: studentsG1["Zdravko", "Petko", "Stanko", "Branko", "Trajko"]
+Test Data:
+Enter student group: (there are 1 and 2 )
+1
+Expected Output:
+The Students in G1 are:
+Zdravko
+Petko
+Stanko
+Branko
+Trajko*/
+
+string[] studentsG6 = new string[] { "Bojan", "Viktor", "Sinisa", "Viktor", "Aleksandra" };
+string[] studentsG1 = new string[] { "Kiril", "Stole", "Kole", "Vlatko", "Kristijan" };
+Console.WriteLine("Enter a number from 1 and 2");
+bool success = int.TryParse(Console.ReadLine(), out int number);
+if(success)
+{
+    if (number == 1)
+    {
+        Console.WriteLine("The Students in G6 are:");
+        foreach (string student in studentsG6)
+        { 
+            Console.WriteLine(student);
+        }
+    }
+    else if (number==2)
+    {
+        Console.WriteLine("The Students in G1 are:");
+        foreach (string student in studentsG1)
+        {
+            Console.WriteLine(student);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid Input!");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid Input!");
+}
---
+++ SumOfEven/Program.cs
@@ -0,0 +1,41 @@
+﻿
+/*Make a console application called SumOfEven. Inside it create an array of 6 integers. Get numbers from the input, find and print the sum of the even numbers from the array:
+
+Test Data:
+Enter integer no.1:
+4
+Enter integer no.2:
+3
+Enter integer no.3:
+7
+Enter integer no.4:
+3
+Enter integer no.5:
+2
+Enter integer no.6:
+8
+Expected Output:
+The result is: 14*/
+
+int[] arrayOfInts = new int[6];
+int sum = 0;
+
+for (int i = 0; i < arrayOfInts.Length; i++)
+{
+    Console.WriteLine("Enter a number for " + i);
+    bool success = int.TryParse(Console.ReadLine(), out int number);
+    if (success)
+    { 
+        arrayOfInts[i] = number;
+    }
+    else
+    {
+        Console.WriteLine("wrong input!");
+    }
+}
+
+foreach(int element in arrayOfInts)
+{
+    sum += element;
+}
+Console.WriteLine("The result is: " + sum);
